f8ea1334fb86b406ce50c3b9944effe43a04ff74
athomasliz <36581159+athomasliz@users.noreply.github.com>
2022-03-20 19:31:15 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,14 +8,14 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://athomasliz.github.io',
+  baseUrl: '/gitwiki/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'athomasliz', // Usually your GitHub org/user name.
+  projectName: 'gitwiki', // Usually your repo name.
+  trailingSlash: false,
   presets: [
     [
       'classic',
