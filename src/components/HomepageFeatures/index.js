b124e42d2403899319bdd3f1bf50fa13fd5584aa
athomasliz <36581159+athomasliz@users.noreply.github.com>
2022-03-22 07:11:03 +0800

    Test

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,32 +4,29 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: 'Who am I',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Thomas Li
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'What is this site?',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        My study note, technical cheatsheets, links...
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Powered by Docusaurus and React',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Thanks to Docusaurus and React
       </>
     ),
   },
