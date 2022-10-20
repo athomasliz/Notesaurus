ef3690f5effccd68d1749fd89fe31401b160a0d7
athomasliz <athomasliz@yahoo.com.hk>
2022-10-20 14:39:44 +0800

    change crypto 101 layout

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,6 +41,10 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      tableOfContents: {
+        minHeadingLevel: 2,
+        maxHeadingLevel: 5,
+      },
       navbar: {
         title: 'Notesaurus',
         logo: {
