4d083a5ba4fed536898e7c124260a7f0729a93ab
athomasliz <athomasliz@yahoo.com.hk>
2022-10-28 12:55:01 +0800

    support mermaid

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,136 +4,143 @@
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
-/** @type {import('@docusaurus/types').Config} */
-const config = {
-  title: 'Keep your notes',
-  tagline: '',
-  url: 'https://athomasliz.github.io',
-  baseUrl: '/Notesaurus/',
-  projectName: 'Notesaurus', // Usually your repo name.
-  organizationName: 'athomasliz', // Usually your GitHub org/user name.
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  themes: ['@docusaurus/theme-live-codeblock'],
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl: 'https://github.com/athomasliz/Notesaurus/tree/main/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl: 'https://github.com/athomasliz/Notesaurus/tree/main/',
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      }),
-    ],
-  ],
+///** @type {import('@docusaurus/types').Config} */
 
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      tableOfContents: {
-        minHeadingLevel: 2,
-        maxHeadingLevel: 5,
-      },
-      navbar: {
-        title: 'Notesaurus',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
-        },
-        items: [
-          {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Notes',
-          },
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/athomasliz/Notesaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
+const createConfig = async () => {
+const mdxMermaid = await import('mdx-mermaid');
+return    {
+      title: 'Keep your notes',
+      tagline: '',
+      url: 'https://athomasliz.github.io',
+      baseUrl: '/Notesaurus/',
+      projectName: 'Notesaurus', // Usually your repo name.
+      organizationName: 'athomasliz', // Usually your GitHub org/user name.
+      onBrokenLinks: 'throw',
+      onBrokenMarkdownLinks: 'warn',
+      favicon: 'img/favicon.ico',
+      themes: ['@docusaurus/theme-live-codeblock'],
+      presets: [
+        [
+          'classic',
+          ///** @type {import('@docusaurus/preset-classic').Options} */
+          ({
+            docs: {
+              sidebarPath: require.resolve('./sidebars.js'),
+              // Please change this to your repo.
+              editUrl: 'https://github.com/athomasliz/Notesaurus/tree/main/',
+              remarkPlugins: [[mdxMermaid.default, {
+                theme: { light: 'neutral', dark: 'forest' }, 
+                themeVariables: { lineColor: '#9CCC65'},
+              }]],
+            },
+            blog: {
+              showReadingTime: true,
+              // Please change this to your repo.
+              editUrl: 'https://github.com/athomasliz/Notesaurus/tree/main/',
+            },
+            theme: {
+              customCss: require.resolve('./src/css/custom.css'),
+            },
+          }),
         ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          /*
-          {
-            title: 'Docs',
+      ],
+
+      themeConfig:
+        /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+        ({
+          tableOfContents: {
+            minHeadingLevel: 2,
+            maxHeadingLevel: 5,
+          },
+          navbar: {
+            title: 'Notesaurus',
+            logo: {
+              alt: 'My Site Logo',
+              src: 'img/logo.svg',
+            },
             items: [
               {
+                type: 'doc',
+                docId: 'intro',
+                position: 'left',
                 label: 'Notes',
-                to: '/docs/intro',
               },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              
+              {to: '/blog', label: 'Blog', position: 'left'},
               {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-              
-            ],
-            
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
+                href: 'https://github.com/athomasliz/Notesaurus',
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                position: 'right',
               },
             ],
           },
-          */
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} Notesaurus. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-        additionalLanguages: ['java', 'yaml'],
-        magicComments: [
-          // Remember to extend the default highlight class name as well!
-          {
-            className: 'theme-code-block-highlighted-line',
-            line: 'highlight-next-line',
-            block: {start: 'highlight-start', end: 'highlight-end'},
+          footer: {
+            style: 'dark',
+            links: [
+              /*
+              {
+                title: 'Docs',
+                items: [
+                  {
+                    label: 'Notes',
+                    to: '/docs/intro',
+                  },
+                ],
+              },
+              {
+                title: 'Community',
+                items: [
+                  
+                  {
+                    label: 'Stack Overflow',
+                    href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+                  },
+                  {
+                    label: 'Discord',
+                    href: 'https://discordapp.com/invite/docusaurus',
+                  },
+                  {
+                    label: 'Twitter',
+                    href: 'https://twitter.com/docusaurus',
+                  },
+                  
+                ],
+                
+              },
+              {
+                title: 'More',
+                items: [
+                  {
+                    label: 'Blog',
+                    to: '/blog',
+                  },
+                  {
+                    label: 'GitHub',
+                    href: 'https://github.com/facebook/docusaurus',
+                  },
+                ],
+              },
+              */
+            ],
+            copyright: `Copyright © ${new Date().getFullYear()} Notesaurus. Built with Docusaurus.`,
           },
-          {
-            className: 'code-block-error-line',
-            line: 'This will error',
+          prism: {
+            theme: lightCodeTheme,
+            darkTheme: darkCodeTheme,
+            additionalLanguages: ['java', 'yaml'],
+            magicComments: [
+              // Remember to extend the default highlight class name as well!
+              {
+                className: 'theme-code-block-highlighted-line',
+                line: 'highlight-next-line',
+                block: {start: 'highlight-start', end: 'highlight-end'},
+              },
+              {
+                className: 'code-block-error-line',
+                line: 'This will error',
+              },
+            ],
           },
-        ],
-      },
-    }),
-};
-
-module.exports = config;
+        }),
+    };
+}
+module.exports = createConfig;
