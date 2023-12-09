103d2de828c08971b4fd80ff6514048d0c917a28
athomasliz <athomasliz@yahoo.com.hk>
2023-12-09 14:22:24 +0800

    core java Hotkey

--- src/components/Highlight.js
+++ src/components/Highlight.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function Highlight({children, bgColor="#000000", fontColor="#ffffff",fontWeight=700, textAlign='center', padding="0.1rem", margin="0rem", border="0", borderRadius="0.5rem", display="inline-block"}) {
+export default function Highlight({children, bgColor="#000000", fontColor="#ffffff",fontWeight=700, textAlign='center', padding="0.1rem", margin="0.05rem", border="0", borderRadius="0.5rem", display="inline-block"}) {
     return (
       <div
         style={{
